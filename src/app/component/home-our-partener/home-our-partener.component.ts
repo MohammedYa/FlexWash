94e6c99333430d3764e653430cb488b83b5154e5
MohammedYa <mohamedel3sawy4110@gmail.com>
2023-11-09 00:56:47 +0200

    add some sections on home  page

---
+++ src/app/component/home-our-partener/home-our-partener.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-our-partener',
+  templateUrl: './home-our-partener.component.html',
+  styleUrls: ['./home-our-partener.component.css']
+})
+export class HomeOurPartenerComponent {
+
+}
