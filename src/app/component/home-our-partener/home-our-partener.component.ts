01a69071d136d67350d7395e5268c6be61a18388
Eman Ahmed <emanamra1999@gmail.com>
2023-11-09 18:35:34 +0200

    home partenership comp

--- src/app/component/home-our-partener/home-our-partener.component.ts
+++ src/app/component/home-our-partener/home-our-partener.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home-our-partener',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class HomeOurPartenerComponent {
 
+  customOptions: OwlOptions = {
+    loop: true,
+    center: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 3
+      }
+    },
+    nav: true
+  }
 }
